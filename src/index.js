f4ea5f451b76f4b20e83cbf0c92c28f7139f8ba5
yasir abdus-sabur <yasirabdussabur@gmail.com>
2023-03-25 14:01:25 -0400

    sound for pads are working

--- src/index.js
+++ src/index.js
@@ -36,25 +36,25 @@ const colors = ["red", "green", "blue", "yellow"];
   {
     color: "red",
     selector: document.querySelector(".js-pad-red"),
-    sound: new Audio("../assets/simon-says-sound-1.mp3"),
+    sound: new Audio("https://github.com/Stelvo/js-dev-final-capstone-starter-simon-says/blob/main/assets/simon-says-sound-1.mp3?raw=true"),
   },
  
   {
     color: "green",
     selector: document.querySelector(".js-pad-green"),
-    sound: new Audio("https://github.com/Stelvo/js-dev-final-capstone-starter-simon-says/blob/main/assets/simon-says-sound-1.mp3?raw=true"),
+    sound: new Audio("https://github.com/Stelvo/js-dev-final-capstone-starter-simon-says/blob/main/assets/simon-says-sound-2.mp3?raw=true"),
   },
 
   {
     color: "blue",
     selector: document.querySelector(".js-pad-blue"),
-    sound: new Audio("../assets/simon-says-sound-3.mp3"),
+    sound: new Audio("https://github.com/Stelvo/js-dev-final-capstone-starter-simon-says/blob/main/assets/simon-says-sound-3.mp3"),
   },
 
   {
     color: "yellow",
     selector: document.querySelector(".js-pad-yellow"),
-    sound: new Audio("../assets/simon-says-sound-4.mp3"),
+    sound: new Audio("https://github.com/Stelvo/js-dev-final-capstone-starter-simon-says/blob/main/assets/simon-says-sound-4.mp3"),
   }
 ];
 
